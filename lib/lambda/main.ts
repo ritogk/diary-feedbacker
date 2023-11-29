19f75ae31fd8f712ff46f2cc0442ec7ae6f2c3e4
ryoji <ryoji@unko.com>
2023-11-29 19:30:22 +0900

    ログ作成

--- lib/lambda/main.ts
+++ lib/lambda/main.ts
@@ -13,22 +13,29 @@ export const handler = async () => {
   )
 
   // 今日日付をyyyy-mm-dd形式で取得する
+  console.log("[st] get today diary")
   const today = new Date()
   const diary = await diaryManager.read(today)
+  console.log("[ed] get today diary")
 
   // フィードバックを受け取る
+  console.log("[st] generate feedback")
   const diaryFeedbacker = new DiaryFeedbacker(env.openaiApiKey)
   const feedback = await diaryFeedbacker.generate(diary.text)
+  console.log("[ed] generate feedback")
 
   // notionページに書き込む
+  console.log("[st] write diary")
   diaryManager.write(
     diary.id,
     feedback.title,
     feedback.feedback,
     feedback.mental
   )
+  console.log("[ed] write diary")
 
   // 通知する
+  console.log("[st] notice")
   const notification = new Notification(env.lineChannelAccessToken)
   const text = `■タイトル
 ${feedback.title}
@@ -48,4 +55,5 @@ ${feedback.positivity.join("\n")}
 ■悪い所
 ${feedback.negativity}`
   notification.notice(env.linePushUserId, text)
+  console.log("[ed] notice")
 }
