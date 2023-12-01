3ce9e92ae9136f31bea7762a6168e9f9e8ccb5b1
ryoji <ryoji@unko.com>
2023-12-01 11:13:55 +0900

    APIGatewayから非同期で実行できるようにした

--- lib/lambda/index.ts
+++ lib/lambda/index.ts
@@ -4,48 +4,62 @@ import { DiaryFeedbacker } from "./core/diary-feedbacker/diary-feedbacker"
 import { Notification } from "./core/notification/notification"
 import * as MessageGenerater from "./core/notification/message-generater"
 
-export const handler = async () => {
-  const envManager = new EnvManger()
-  const env = await envManager.getEnv()
+export const handler = async (event: any) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const envManager = new EnvManger()
+      const env = await envManager.getEnv()
 
-  const diaryManager = new DiaryManager(
-    env.notionSecret,
-    env.notionDiaryDatabaseId
-  )
+      const diaryManager = new DiaryManager(
+        env.notionSecret,
+        env.notionDiaryDatabaseId
+      )
 
-  // 今日日付の日記を取得する
-  console.log("[st] fetch today diary")
-  const today = new Date()
-  await diaryManager.load(today)
-  const diary = diaryManager.getDiary()
-  if (diary === "") {
-    console.log("[message] empty diary")
-    return
-  }
-  console.log("[ed] fetch today diary")
+      // 今日日付の日記を取得する
+      console.log("[st] fetch today diary")
+      const today = new Date()
+      await diaryManager.load(today)
+      const diary = diaryManager.getDiary()
+      if (diary === "") {
+        console.log("[message] empty diary")
+        reject("diary not found")
+      }
+      console.log("[ed] fetch today diary")
 
-  // 日記のフィードバックを受け取る
-  console.log("[st] generate feedback")
-  const diaryFeedbacker = new DiaryFeedbacker(env.openaiApiKey)
-  const feedback = await diaryFeedbacker.generate(diary)
-  console.log("[ed] generate feedback")
+      // 日記のフィードバックを受け取る
+      console.log("[st] generate feedback")
+      const diaryFeedbacker = new DiaryFeedbacker(env.openaiApiKey)
+      const feedback = await diaryFeedbacker.generate(diary)
+      console.log("[ed] generate feedback")
 
-  // 日記にフィードバックを書き込む
-  console.log("[st] write diary")
-  diaryManager.write(
-    feedback.title,
-    feedback.summary,
-    feedback.mental,
-    feedback.feedback,
-    feedback.suggestion,
-    feedback.positivity.join(", "),
-    feedback.negativity
-  )
-  console.log("[ed] write diary")
+      // 日記にフィードバックを書き込む
+      console.log("[st] write diary")
+      diaryManager.write(
+        feedback.title,
+        feedback.summary,
+        feedback.mental,
+        feedback.feedback,
+        feedback.suggestion,
+        feedback.positivity.join(", "),
+        feedback.negativity
+      )
+      console.log("[ed] write diary")
 
-  // 通知する
-  console.log("[st] notice")
-  const notification = new Notification(env.lineChannelAccessToken)
-  notification.notice(env.linePushUserId, MessageGenerater.generate(feedback))
-  console.log("[ed] notice")
+      // 通知する
+      console.log("[st] notice")
+      const notification = new Notification(env.lineChannelAccessToken)
+      await notification.notice(
+        env.linePushUserId,
+        MessageGenerater.generate(feedback)
+      )
+      console.log("[ed] notice")
+
+      resolve({
+        statusCode: 200,
+        body: JSON.stringify("success"),
+      })
+    } catch (error) {
+      reject(error)
+    }
+  })
 }
